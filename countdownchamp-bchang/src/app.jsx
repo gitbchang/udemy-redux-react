7dca0e82d34542d26bcaf7c94b26cb6168146b12
Brian Chang <brian.h.chang1@gmail.com>
2017-04-09 10:40:19 -0500

    react-bootstrap changes

--- countdownchamp-bchang/src/app.jsx
+++ countdownchamp-bchang/src/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Clock from './Clock';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +14,8 @@ class App extends Component {
 
   changeDeadline = () => {
     this.setState({deadline: this.state.newDeadline});
+    this.refs.dateSubmit.value='';
+    
   }
 
   render() {
@@ -22,12 +25,13 @@ class App extends Component {
         <Clock 
           deadline={this.state.deadline}
         />
-        <div>
-          <input
+        <Form inline='true'>
+          <FormControl
+            ref='dateSubmit'
             placeholder='new date'
             onChange={event => { this.setState({ newDeadline: event.target.value }); } }/>
-          <button onClick={this.changeDeadline}>Submit</button>
-        </div>
+          <Button onClick={this.changeDeadline}>Submit</Button>
+        </Form>
       </div>
     );
   }
