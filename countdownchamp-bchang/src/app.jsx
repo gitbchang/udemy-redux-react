4ed5b24378acede0583636637e895a035dea4c41
Brian Chang <brian.h.chang1@gmail.com>
2017-04-09 21:10:12 -0500

    stopwatch added

--- countdownchamp-bchang/src/app.jsx
+++ countdownchamp-bchang/src/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Clock from './Clock';
+import Stopwatch from './Stopwatch';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
@@ -8,18 +9,19 @@ class App extends Component {
     super(props);   
     this.state = {
       deadline: 'December 25, 2017',
-      newDeadline: ''
+      newDeadline: '',
+      stopwatchTimer: '',
+      newStopwatchTimer: ''
     }
   }
 
-  // formPreventDefault = e => {
-  //   e.preventDefault();
-  // }
-
   changeDeadline = () => {
     this.setState({deadline: this.state.newDeadline});
-    this.refs.dateSubmit.value='';
-    
+    this.refs.dateSubmit.value='';    
+  }
+
+  changeStopwatch = () => {
+    this.setState({stopwatchTimer: this.state.newStopwatchTimer });
   }
 
   render() {
@@ -29,13 +31,23 @@ class App extends Component {
         <Clock 
           deadline={this.state.deadline}
         />
+        <Stopwatch 
+          timer={this.state.stopwatchTimer}
+        />
         <Form onSubmit={e => e.preventDefault()} inline={true}>
           <FormControl
             className='Deadline-input'
             ref='dateSubmit'
-            placeholder='new date'
+            placeholder='New Date'
             onChange={event => { this.setState({ newDeadline: event.target.value }); } }/>
           <Button onClick={this.changeDeadline}>Submit</Button>
+          <br/>
+          <FormControl
+            className='Deadline-input'
+            ref='stopwatchSubmit'
+            placeholder='New Timer'
+            onChange={event => {this.setState({ newStopwatchTimer: event.target.value}); } } />
+          <Button onClick={this.changeStopwatch} >Submit</Button>
         </Form>
       </div>
     );
