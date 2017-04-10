455fe02f1120f2e6efec87e36b7ef7675501061e
Brian Chang <brian.h.chang1@gmail.com>
2017-04-10 18:41:07 -0500

    pause play functionality

---
+++ musicmaster-bchang/src/Gallery.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+  }
+
+  playAudio(previewUrl) {
+    let audio = new Audio(previewUrl);
+    audio.play();
+    if(!this.state.playing) {
+      audio.play();
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio: audio
+      });
+    } else {
+        if (this.state.playingUrl === previewUrl) {
+          this.state.audio.pause();
+          this.setState({
+            playing: false
+          })
+        } else {
+          this.state.audio.pause();
+          audio.play();
+          this.setState({
+            playingUrl: previewUrl,
+            playing: true,
+            audio: audio
+          })
+      }
+    }
+  }
+
+  render() {
+    
+    const tracks = this.props.tracks;
+
+    return (
+      <div>
+        {tracks.map((track, k) => {
+          const trackImg = track.album.images[0].url;
+          console.log('track', track);
+          return (
+            <div key={k} className='track' onClick={() => this.playAudio(track.preview_url)} >
+              <img src={trackImg} className='track-img' alt="track"/>
+              <p className='track-text'>{track.name}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Gallery;
